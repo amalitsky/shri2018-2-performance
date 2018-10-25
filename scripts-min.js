a9e0a376c0e4b15f204fd82ca23dd82b7ef1a00f
Alex Malitsky <a.malitsky@gmail.com>
2018-10-25 23:46:39 +0300

    Update 2018-10-25T20:46:35.884Z

--- scripts-min.js
+++ scripts-min.js
@@ -240,9 +240,9 @@ arrowLeftScens.addEventListener('click', function() {
   if (currentPage > 1) {
     currentPage -= 1;
     arrowRightScens.classList.toggle('paginator__arrow_disabled',
-        currentPage === pageCountScens);
+      currentPage === pageCountScens);
     arrowLeftScens.classList.toggle('paginator__arrow_disabled',
-        currentPage === 1);
+      currentPage === 1);
     scenarios.scroll({
       top: 0,
       left: -645,
@@ -253,7 +253,7 @@ arrowLeftScens.addEventListener('click', function() {
 
 const selectButton = document.querySelector('.filter__select-button');
 const selectButtonText = document.querySelector(
-    '.filter__select-button .button__text');
+  '.filter__select-button .button__text');
 const selectOptions = document.querySelectorAll('.filter__select-item');
 const popup = document.querySelector('.filter__select-popup');
 
@@ -261,18 +261,12 @@ selectButton.addEventListener('click', function() {
   popup.classList.toggle('filter__select-popup_open');
 });
 
-/*let widths = '';
-window.addEventListener('scroll', function() {
-  widths += document.querySelectorAll('body')[0].offsetWidth;
-  document.querySelector('.stats').innerHTML = widths;
-});*/
-
 selectOptions.forEach(o => {
   o.addEventListener('click', function(e) {
     document.querySelector('#' + e.target.dataset.group).checked = true;
 
     selectOptions.forEach(
-        opt => opt.classList.toggle('filter__select-item_checked', false));
+      opt => opt.classList.toggle('filter__select-item_checked', false));
     e.target.classList.toggle('filter__select-item_checked', true);
     popup.classList.toggle('filter__select-popup_open', false);
     selectButtonText.innerText = e.target.innerText;
